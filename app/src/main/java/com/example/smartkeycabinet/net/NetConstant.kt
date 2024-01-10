18dd05cad329053c5c7a2b95e82794bae40d062b
Nero <835752247@qq.com>
2024-01-10 10:22:42 +0800

    添加注释

--- app/src/main/java/com/example/smartkeycabinet/net/NetConstant.kt
+++ app/src/main/java/com/example/smartkeycabinet/net/NetConstant.kt
@@ -2,8 +2,10 @@ package com.example.smartkeycabinet.net
 
 
 object NetConstant {
+    //测试环境
     const val TEST_SERVER_URL = "http://114.116.231.107:9090/inner-api/car/"
 //    const val TEST_SERVER_URL = "http://192.168.1.102:8089/"
 //    const val RELEASE_SERVER_URL = "http://prod-cn.your-api-server.com/"
+    //正式环境
     const val RELEASE_SERVER_URL = "http://56.35.6.198:8090/inner-api/car/"
 }
