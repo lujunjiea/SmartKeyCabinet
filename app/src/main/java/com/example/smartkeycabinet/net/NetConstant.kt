7e8f70957f57774691241ec12f9277e1c3e99e39
Nero <835752247@qq.com>
2024-01-11 13:43:49 +0800

    正式环境IP变更

--- app/src/main/java/com/example/smartkeycabinet/net/NetConstant.kt
+++ app/src/main/java/com/example/smartkeycabinet/net/NetConstant.kt
@@ -7,5 +7,6 @@ object NetConstant {
 //    const val TEST_SERVER_URL = "http://192.168.1.102:8089/"
 //    const val RELEASE_SERVER_URL = "http://prod-cn.your-api-server.com/"
     //正式环境
-    const val RELEASE_SERVER_URL = "http://56.35.6.198:8090/inner-api/car/"
+//    const val RELEASE_SERVER_URL = "http://56.35.6.198:8090/inner-api/car/"
+    const val RELEASE_SERVER_URL = "http://56.35.6.196:8090/inner-api/car/"
 }
